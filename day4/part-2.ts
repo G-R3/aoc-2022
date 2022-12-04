30322a7d146f71ea545f5f841d8551b86c44a8d0
Gerardo <gr261212@gmail.com>
2022-12-04 13:22:21 -0500

    day 4

---
+++ day4/part-2.ts
@@ -0,0 +1,25 @@
+const input = await Deno.readTextFile("input.txt");
+
+let total = 0;
+
+const fillArray = (min: number, max: number) => {
+  const arr = [];
+  for (let i = min; i <= max; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+input.split("\n").forEach((line) => {
+  const [pairLeft, pairRight] = line.split(",");
+
+  const leftRange = pairLeft.split("-").map((n) => parseInt(n, 10));
+  const rightRange = pairRight.split("-").map((n) => parseInt(n, 10));
+
+  const left = fillArray(leftRange[0], leftRange[1]);
+  const right = fillArray(rightRange[0], rightRange[1]);
+
+  if (left.some((n) => right.includes(n))) total++;
+});
+
+console.log(total);
