e11bcefa62f4cb21bff42ce401c55b2792ee8153
Gerardo <gr261212@gmail.com>
2022-12-11 15:45:58 -0500

    :skull:

---
+++ day5/part-2.ts
@@ -0,0 +1,50 @@
+const input = await Deno.readTextFile("input.txt");
+
+const stacks: string[][] = [];
+
+const [firstHalf, secondHalf] = input.split("\n\n");
+
+const numOfStacks = Number(firstHalf.split("\n").reverse()[0].slice(-2));
+const crates = firstHalf.split("\n").reverse().slice(1);
+
+const instructions = secondHalf.split("\n").map((instruciton) => {
+  const steps = instruciton.split(" ").map((step) => Number(step));
+
+  return [steps[1], steps[3], steps[5]];
+});
+
+for (let i = 0; i < numOfStacks; i++) {
+  stacks[i] = [];
+}
+
+crates.forEach((crate) => {
+  let position = 0;
+
+  for (let i = 0; i < numOfStacks; i++) {
+    position = i == 0 ? position + 1 : position + 3;
+
+    if (crate.charAt(position) != " ") stacks[i].push(crate.charAt(position));
+    position++;
+  }
+});
+
+instructions.forEach((instruction) => {
+  const [amount, from, to] = instruction;
+
+  if (amount === 1) {
+    for (let i = 0; i < amount; i++) {
+      stacks[to - 1].push(stacks[from - 1].pop()!);
+    }
+  } else {
+    const toMove = [];
+    for (let i = 0; i < amount; i++) {
+      toMove.push(stacks[from - 1].pop()!);
+    }
+
+    stacks[to - 1].push(...toMove.reverse());
+  }
+});
+
+const result = stacks.map((stack) => stack[stack.length - 1]).join("");
+
+console.log(result);
