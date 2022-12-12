edd4d9e1bff9ca74a206132fc5be359773993224
Gerardo <gr261212@gmail.com>
2022-12-11 21:01:01 -0500

    day 7

---
+++ day7/part-1.ts
@@ -0,0 +1,98 @@
+const input = await Deno.readTextFile("input.txt");
+
+const lines = input.split("\n");
+
+type Directory = {
+  name: string;
+  parent?: Directory;
+  files: { [key: string]: string };
+  directories: { [key: string]: Directory };
+};
+
+const rootDirector: Directory = {
+  name: "/",
+  files: {},
+  directories: {},
+};
+
+let current: Directory = rootDirector;
+
+for (const line of lines) {
+  const command = line.split(" ");
+  if (command[0] === "$") {
+    if (command[1] === "cd") {
+      if (command[2] === "/") {
+        // Go to root directory
+        current = rootDirector;
+      } else if (command[2] === "..") {
+        // Go back to parent directory
+        if (current.parent) {
+          current = current.parent;
+        }
+      } else {
+        // Go to child directory
+        const dir = command[2];
+        if (!current.directories[dir]) {
+          current.directories[dir] = {
+            name: dir,
+            parent: current,
+            files: {},
+            directories: {},
+          };
+        }
+        current = current.directories[dir];
+      }
+    }
+  } else {
+    if (command[0] === "dir") continue;
+
+    current.files[command[1]] = command[0];
+  }
+}
+
+/*
+ / 
+    /a
+        /e
+            - 584 i
+        - 29116 f
+        - 2557 g
+        - 62596 h.lst
+    - 14848514 b.txt
+    - 8504156 c.dat
+    /d
+        - 4060174 j
+        - 8033020 d.log
+        - 5626152 d.ext
+        - 7214296 k
+
+*/
+
+const fileSizes: number[] = [];
+
+// calculate file size
+function calculateSizes(dir: Directory): number {
+  let size = 0;
+
+  for (const file in dir.files) {
+    size += parseInt(dir.files[file]);
+  }
+
+  for (const directory in dir.directories) {
+    const directorySize = calculateSizes(dir.directories[directory]);
+
+    size += directorySize;
+
+    fileSizes.push(directorySize);
+  }
+
+  return size;
+}
+
+calculateSizes(rootDirector);
+
+console.log(fileSizes);
+
+console.log(
+  fileSizes.filter((size) => size <= 100000).reduce((a, b) => a + b, 0),
+);
